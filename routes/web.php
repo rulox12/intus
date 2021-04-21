a2e1b1f68228bc6a2f89116795e49cf79c18c15c
Daniel Betancur <daniel.betancur@evertecinc.com>
2021-04-20 22:23:10 -0500

    feat: The routes for the management of votes questions and answers are created

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/questions','App\Http\Controllers\QuestionController');
+Route::resource('/answers','App\Http\Controllers\AnswerController');
+Route::get('/votes', 'App\Http\Controllers\VoteController@index')->name('votes.index');
+Route::get('/votes/create/{question}', 'App\Http\Controllers\VoteController@create')->name('votes.create');
+Route::get('/votes/store/{question_id}/{answer_id}', 'App\Http\Controllers\VoteController@store')->name('votes.store');
+Route::get('/questions/all', 'App\Http\Controllers\QuestionController@all')->name('questions.all');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
